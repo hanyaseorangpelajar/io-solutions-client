2d7ef49a07c08aed4df07ac0a28df349dfe9c2fd
hanyaseorangpelajar <adriaudifirst@students.undip.ac.id>
2025-11-08 16:12:35 +0700

    fix(admin): fixing admin dashboard

--- src/features/tickets/index.ts
+++ src/features/tickets/index.ts
@@ -1,20 +1,14 @@
-// src/features/tickets/index.ts
-
-// ---- Model
 export * from "./model/types";
 export * from "./model/schema";
 
-// ---- Pages (List & History = named; lainnya default)
-export { TicketsListPage } from "./ui/TicketsListPage";
-export { TicketsHistoryPage } from "./ui/TicketsHistoryPage";
+export { default as TicketsListPage } from "./ui/TicketsListPage";
+
 export { default as TicketsMyWorkPage } from "./ui/TicketsMyWorkPage";
 export { default as TicketDetailPage } from "./ui/TicketDetailPage";
 
-// ---- UI components
 export { default as ResolveTicketModal } from "./ui/ResolveTicketModal";
 export { default as TicketFormModal } from "./ui/TicketFormModal";
 export { default as TicketPriorityBadge } from "./ui/TicketPriorityBadge";
 export { default as TicketStatusBadge } from "./ui/TicketStatusBadge";
 
-// ---- Utils
 export * from "./utils/format";
